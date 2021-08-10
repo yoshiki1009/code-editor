87a8a1d1bf1a0874e12939b388ec79171a9fc869
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-08-09 21:55:15 -0700

    implement multiple editor

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+import Resizable from './resizable';
+
+const CodeCell = () => {
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <Resizable direction="vertical">
+      <div>
+        <CodeEditor
+          initialValue="const a = 1"
+          onChange={(value) => setInput(value)}
+        />
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
