b6647ad9d29b813326e37d4672ba88d3001603e2
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-08-16 09:56:32 -0700

    implement redux

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -5,6 +5,7 @@ import './text-editor.css';
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
 
   useEffect(() => {
     // to back preview mode when user click outside of editor
@@ -30,14 +31,16 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     );
   }
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
