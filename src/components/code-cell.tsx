19f089f398059a8f9b79ae0508b7d7b273db6dfe
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-08-28 15:16:58 -0700

    add bundle function

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,33 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundle from '../bundler';
+// import bundle from '../bundler';
 import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState('');
-  const [err, setErr] = useState('');
+  //replace local state to redux store
+  // const [code, setCode] = useState('');
+  // const [err, setErr] = useState('');
   // const [input, setInput] = useState('');replace update cell ↓
-  const { updateCell } = useActions();
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  // console.log(bundle);
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       // const output = await bundle(input);
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setErr(output.err);
+      // const output = await bundle(cell.content);
+      // setCode(output.code);
+      // setErr(output.err);
+      createBundle(cell.id, cell.content);
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content]);
+  }, [cell.content, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
@@ -44,7 +49,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)} // onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} err={err} />
+        {bundle && <Preview code={bundle.code} err={bundle.err} />}
       </div>
     </Resizable>
   );
