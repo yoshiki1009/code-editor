2a602c982fdf69a3b1d6655596ebeba2549bbd58
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-08-30 15:30:28 -0700

    implement cumulative code feature

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -20,10 +20,42 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
   // console.log(bundle);
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+    const orderedCells = order.map((id) => data[id]);
+
+    const code = [
+      `
+      const show = (value) =>{
+        const root = document.querySelector('#root')
+
+        if(typeof value === 'object'){
+          if(value.$$typeof && value.props){
+            ReactDOM.render(value, root)
+          }else{
+            root.innerHTML = JSON.stringify(value);
+          }
+        }else{
+          root.innerHTML = value;
+        }
+      }
+      `,
+    ];
+    for (let c of orderedCells) {
+      if (c.type === 'code') {
+        code.push(c.content);
+      }
+      if (c.id === cell.id) {
+        // stop looping when loop come current cell
+        break;
+      }
+    }
+    return code;
+  });
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
       return;
     }
     const timer = setTimeout(async () => {
@@ -31,14 +63,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       // const output = await bundle(cell.content);
       // setCode(output.code);
       // setErr(output.err);
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
+      // console.log(cumulativeCode.join('\n'));
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
